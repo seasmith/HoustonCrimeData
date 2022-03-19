e40e4133b3cc6eb5be039d4fb1fd25ce0461e23a
Luke Smith <lukesmith@Lukes-MacBook-Air.local>
2022-03-18 20:50:06 -0500

    fix invalid geo and extend population interpolation to 2025

--- scripts/tidy/03_grided-population-data_extract.R
+++ scripts/tidy/03_grided-population-data_extract.R
@@ -5,6 +5,7 @@ library(units)
 # Use Houston Police beats for get necessary population points
 hp_beats <- st_read("data/Houston_Police_Beats/Houston_Police_Beats.shp")
 hp_beats <- mutate(hp_beats, Area_sq_mi = set_units(Area_sq_mi, mi^2))
+hp_beats <- st_make_valid(hp_beats)  # apparently 05F40 is in valid (vertex dupe)
 # There are a lot of points (500k+) in the population data.
 ref <- "data/GPW_TX"
 is_east_shp <- grepl("_east$", st_layers(ref)[["name"]])
@@ -38,7 +39,8 @@ fill_gaps <- function(y1, y2, var1, var2) {
 
 hou_pop <- hou_pop %>%
  mutate(!!!fill_gaps(2010, 2015, UN_2005_E, UN_2010_E),
-        !!!fill_gaps(2015, 2020, UN_2010_E, UN_2015_E))
+        !!!fill_gaps(2015, 2020, UN_2010_E, UN_2015_E),
+        !!!fill_gaps(2020, 2025, UN_2015_E, UN_2020_E))
 
 
 hou_pop <- hou_pop %>%
@@ -48,7 +50,7 @@ hou_pop <- hou_pop %>%
  summarize_at(vars(matches("^UN_20[0-9]+_E$")), sum, na.rm = TRUE)
 
 hou_pop <- hou_pop %>%
- inner_join(select(as_data_frame(hp_beats), Beats, Area_sq_mi), by = "Beats")
+ inner_join(select(as_tibble(hp_beats), Beats, Area_sq_mi), by = "Beats")
 
 hou_pop <- hou_pop %>%
  pivot_longer(cols = -c(Beats, Area_sq_mi), names_to = "key", values_to = "value") %>%

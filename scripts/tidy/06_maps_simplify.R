de4bc9edf3f97a578bbd389d941831a7bc0e7cbf
Luke Smith <lukesmith@Lukes-MacBook-Air.local>
2022-03-18 20:50:52 -0500

    change pb to bp -- typo

--- scripts/tidy/06_maps_simplify.R
+++ scripts/tidy/06_maps_simplify.R
@@ -5,6 +5,6 @@ library(sf)
 pb_file <- "data/Houston_Police_Beats/Houston_Police_Beats.shp"
 pb <- st_read(pb_file)
 
-map_pol_beat_simp <- ms_simplify(bp, keep = 0.025, weighting = 0.8)
+map_pol_beat_simp <- ms_simplify(pb, keep = 0.025, weighting = 0.8)
 
 save(map_pol_beat_simp, file = "data/map_pol_beat_simp.RData")
